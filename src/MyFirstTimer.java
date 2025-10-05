25e1265b7efc8fd8168fa2f25b5a2beb5376708a
Lei'lani Garcia <d_garcia42@u.pacific.edu>
2025-10-05 13:36:57 -0700

    timer needs javax.swing library, time in ms and who the timer wakes up

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,16 @@
 import acm.graphics.*;
 import acm.program.*;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class MyFirstTimer extends GraphicsProgram {
+public class MyFirstTimer extends GraphicsProgram implements ActionListener{
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	private int numTimes;
+	private Timer t = new Timer(1000, this);
+	
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -13,9 +18,25 @@ public class MyFirstTimer extends GraphicsProgram {
 	}
 	
 	public void run() {
+		numTimes = 0;
+		Timer someTimerVar = new Timer(1000, this);
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		//Timer t = new Timer(1000, this);
+		t.setInitialDelay(3000);
+		t.start();
 	}
+	
+	public void actionPerformed(ActionEvent e) {
+		//add what we want to do every second
+		myLabel.move(5, 0);
+		numTimes++;
+		myLabel.setLabel("number of times called? " + numTimes);
+		if(numTimes == 10) {
+			t.stop();
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		new MyFirstTimer().start();
